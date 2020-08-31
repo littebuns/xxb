9e11a95e53013a9053bd64bd26d5970d66241b97
xuxinbo <xuxinbo@beyondcent.com>
2020-08-31 19:03:20 +0800

    post请求 带参数

--- src/main/java/com/example/demo/utils/httpClient/post.java
+++ src/main/java/com/example/demo/utils/httpClient/post.java
@@ -4,12 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
@@ -21,15 +19,19 @@ import java.util.List;
 @Slf4j
 public class post {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //创建一个httpClient
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        String url = "http://localhost:55006/api/csp/v1/upcdb/combo_groups?params=[{\"param\":{\"is_enabled\":true},\"sign\":\"EQ\"},{\"param\":{\"type\":\"UPDRDB\"},\"sign\":\"EQ\"}]";
+        String url = "http://localhost:55006/api/csp/v1/upcdb/combo_groups";
         HttpPost httpPost = new HttpPost(url);
         CloseableHttpResponse response = null;
         try {
+            httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+//            httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
+            //k v传参
             List<NameValuePair> list = new ArrayList<>();
-            list.add(new BasicNameValuePair("params","[{\"param\":{\"is_enabled\":true},\"sign\":\"EQ\"},{\"param\":{\"type\":\"UPDRDB\"},\"sign\":\"EQ\"}]"));
+            list.add(new BasicNameValuePair("params", "[{\"param\":{\"is_enabled\":true},\"sign\":\"EQ\"},{\"param\":{\"type\":\"UPDRDB\"},\"sign\":\"EQ\"}]"));
             UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list, "utf-8");
             httpPost.setEntity(entity);
             // 由客户端执行(发送)Post请求
@@ -37,12 +39,9 @@ public class post {
             // 从响应模型中获取响应实体
             HttpEntity responseEntity = response.getEntity();
 
-            log.info("响应状态",response.getStatusLine());
-
-            if (responseEntity != null) {
-                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
-                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
-            }
+            log.info("响应状态:" + response.getStatusLine());
+            String result = EntityUtils.toString(responseEntity, "utf-8");
+            log.info("返回结果:" + result);
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         } finally {
