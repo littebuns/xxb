bc456650979bacc912f662c8bac795be86471e0c
xuxinbo <xuxinbo@beyondcent.com>
2021-01-06 11:08:29 +0800

    代理以及使用组合和继承

---
+++ src/main/java/com/example/demo/base/java/复用类7/代理73/SpaceShipControls.java
@@ -0,0 +1,21 @@
+package com.example.demo.base.java.复用类7.代理73;
+
+/**
+ * @author HP
+ */
+public class SpaceShipControls {
+
+    void up(int velocity){}
+
+    void down(int veloctiy){}
+
+    void left(int veloctiy){}
+
+    void right(int velocity){}
+
+    void forward(int veloctiy){}
+
+    void back(int veloctiy){}
+
+    void turboBoost(){}
+}
