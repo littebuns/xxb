518454d5fd8326583327dca142d2d7a0eb52b71b
xuxinbo <xuxinbo@beyondcent.com>
2021-01-21 12:00:53 +0800

    构造器初始化

---
+++ src/main/java/com/example/demo/base/java/初始化与清理5/构造器初始化57/Counter.java
@@ -0,0 +1,15 @@
+package com.example.demo.base.java.初始化与清理5.构造器初始化57;
+
+public class Counter {
+    int i;
+
+    public Counter() {
+        System.out.println(i);
+        i = 7;
+    }
+
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        System.out.println(counter.i);
+    }
+}
